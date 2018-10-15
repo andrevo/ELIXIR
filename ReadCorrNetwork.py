47c1123dc0f939166a3fce97fb4c6d3008d195e4
Andre Voigt <andre.voigt@ntnu.no>
2018-10-15 16:46:53 -0700

    Added support for adding flags, and converted to Python 3 compatible syntax

--- ReadCorrNetwork.py
+++ ReadCorrNetwork.py
@@ -10,7 +10,6 @@ parser.add_argument('-p', type=float)
 parser.add_argument('-f')
 args = vars(parser.parse_args())
 
-print(args)
 
 
 fname = 'inFile.txt'
@@ -22,12 +21,13 @@ f = open(fname)
 network = nx.Graph()
 
 edges = {}
+edgeVals = []
 
 for line in f:
     splitLine = line.rstrip().split('\t')
     edges[(splitLine[0], splitLine[1])] = float(splitLine[2])
+    edgeVals.append(float(splitLine[2]))
 
-edgeVals = edges.values()
 edgeVals.sort(reverse=True)
 cutoffIndex = int(float(len(edgeVals))*0.01*args['p'])
 cutoff = edgeVals[cutoffIndex]
@@ -37,22 +37,28 @@ for edge in edges:
     if edges[edge] > cutoff:
         network.add_edge(edge[0], edge[1], weight=edges[edge])
 
-if args['e']:
-    evc = nx.eigenvector_centrality(network)
-if args['d']:
-    degs = nx.degree(network)
-if args['c']:
-    clus = nx.clustering(network)
+nodeFunc = {'e': nx.eigenvector_centrality, 'd': nx.degree, 'c': nx.clustering}
+nodeFuncNames = {'e': 'Eigenvector centrality', 'd': 'Degree', 'c': 'Clustering'}
+nodeRes = {}
 
+
+for arg in nodeFunc:
+    if args[arg]:
+        nodeRes[arg] = nodeFunc[arg](network)
+
+    
+oline='Node\t'
+for arg in nodeRes:
+    if args[arg]:
+        oline = oline+nodeFuncNames[arg]+'\t'
+print(oline)
+        
 for node in network:
     oline = str(node)+'\t'
-    if args['e']:
-        oline = oline+str(evc[node])+'\t'
-    if args['d']:
-        oline = oline+str(degs[node])+'\t'
-    if args['c']:
-        oline = oline+str(clus[node])+'\t'
-    oline = oline.rstrip()
-    print oline
+    for arg in nodeRes:
+        if args[arg]:
+            oline = oline+str(nodeRes[arg][node])+'\t'
+    oline.rstrip()
+    print(oline)
         
     
